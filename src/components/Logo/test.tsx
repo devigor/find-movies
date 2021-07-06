39727a80c38209bdb6f877691bf8c6386dcd502f
Igor Oliveira <devigor.tere@gmail.com>
2021-07-06 11:18:06 -0300

    test(logo/test): add two tests for logo component
    First test is verify the title be in the document
    
     The second test verify the logo image exist

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-
+import LogoImg from 'assets/images/PlayIcon.png'
 import { Logo } from '.'
 
 describe('<Logo />', () => {
@@ -7,5 +7,10 @@ describe('<Logo />', () => {
     render(<Logo />)
 
     expect(screen.getByText(/Movies4Fun/i)).toBeInTheDocument()
+  })
+
+  it('Should be render the icon play image', () => {
+    render(<Logo />)
+    expect(screen.getByRole('img')).toHaveAttribute('src', LogoImg)
   })
 })
