d08e2d6470e04e42cd759a442b747db2c5f349a6
Igor Oliveira <devigor.tere@gmail.com>
2021-07-06 10:18:08 -0300

    refactor(container): remove the justify content and align items and change the background color

--- src/styles/Container.ts
+++ src/styles/Container.ts
@@ -5,8 +5,5 @@ export const Container = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-  background-color: #e9e9e9;
+  background-color: #444444;
 `
