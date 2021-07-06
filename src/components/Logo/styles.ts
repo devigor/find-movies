856f1857fa5b9b014c316b0ed2620f9b85e86490
Igor Oliveira <devigor.tere@gmail.com>
2021-07-06 11:16:52 -0300

    refactor(logo/styles): refactor the styles

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.header`
   width: 100%;
@@ -10,17 +11,23 @@ export const LinkHome = styled.a`
   justify-content: center;
   text-decoration: none;
   align-items: center;
+
+  ${media.greaterThan('medium')`
+    justify-content: right;
+    margin-left: 2rem;
+  `}
 `
 
 export const Title = styled.h1`
   color: #e2e2e2;
-  font-size: 1.4rem;
+  font-size: 1.8rem;
   font-family: 'Inter', sans-serif;
   font-weight: 400;
+  text-transform: uppercase;
   padding-left: 1rem;
 `
 
 export const ImgLogo = styled.img`
-  width: 2rem;
-  height: 2rem;
+  width: 3rem;
+  height: 3rem;
 `
